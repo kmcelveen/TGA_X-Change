351752cbd9812c9cbcf0014cddfa6d429172b16a
Juan <jguardado85@gmail.com>
2016-09-02 20:20:25 -0700

    fixed some linting (auto fix) and moved script tag

--- public/components/BookSection.js
+++ public/components/BookSection.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
 class BookSection extends React.Component {
-    constructor(props) {
-        super(props);
-        this.displayName = 'BookSection';
-    }
-    render() {
-        return (
-          <div>
-            <div id="booksection">
-              <h2>BookSection</h2>
-            </div>
-          <div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.displayName = 'BookSection';
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="booksection">
+          <h2>BookSection</h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default BookSection;
